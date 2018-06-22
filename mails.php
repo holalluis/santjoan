a1ed324b4d6a15c4654bf8d2fc647c0f17370f2c
holalluis <holalluis@gmail.com>
2018-06-22 22:34:48 +0200

    canvis

--- mails.php
+++ mails.php
@@ -4,6 +4,9 @@
 <?php include'navbar.php'?>
 <h3>Mails</h3><hr>
 <?php
+  if(!$admin)die("No ets admin");
+
+  //consulta tots els mails
   $sql="SELECT * FROM assistents";
   $res=$mysql->query($sql) or die(mysqli_error($mysql));
   while($row=mysqli_fetch_assoc($res)){
@@ -15,7 +18,7 @@
     //check if mail has arroba
     if(strpos($mail,'@')){
       echo "<div>
-        $mail
+        $mail,
       </div>";
     }
   }
