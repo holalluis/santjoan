e238be162b153e71f99483f3082ad51be6acfc1e
holalluis <holalluis@gmail.com>
2019-06-19 03:02:09 +0200

    canvis

--- btn_join.php
+++ btn_join.php
@@ -7,7 +7,7 @@
 
   <div id=data_limit>
     &#9201;
-    Data límit per <b>PAGAR</b>: diumenge 16 de juny!
+    Data límit per <b>PAGAR</b>: AVUI! ESPAVILA! :)
     <style>
       /*animacio*/
       @keyframes blink { from { opacity:0.2; } to { opacity:1; } }
