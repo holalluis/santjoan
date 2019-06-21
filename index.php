413df6fcf0b3eb41d9f94e7c4f240abe89df7a87
holalluis <holalluis@gmail.com>
2019-06-21 23:04:47 +0200

    canvis

--- index.php
+++ index.php
@@ -13,13 +13,15 @@
   </p>
   <p>
     <b>Sentiu-vos partíceps i amfitrions de la festa, que és vostra</b>.
-    No sou al Sònar ni a L'Apol·lo, ningú es lucra amb la vostra celebració i això no té preu.
+    No sou al Sònar ni a L'Apol·lo: ningú es lucra amb la vostra celebració i això no té preu.
     O un de molt petit: <b>us demanem que sigueu actius en cooperar perquè tot surti bé</b>
-    (des del muntar, durant el transcurs de la festa i sobretot al desmuntar).
-    Un cop s'acabi la música s'haurà de portar tot el material electrònic a les furgonetes,
-    plegar les taules i la carpa i fer una primera recollida de brossa.
-    I quan la ressaca ens comenci a despertar haurem de fer una altra brigada
-    per acabar de deixar la platja neta perquè hi puguem tornar un any més!
+    (des de muntar durant el transcurs de la festa i <b>sobretot al desmuntar</b>).
+  </p><p>
+    Un cop s'acabi la música s'haurà de portar tot el material electrònic a les
+    furgonetes, plegar les taules i la carpa i fer una primera recollida de
+    brossa. I quan la ressaca ens comenci a despertar haurem de fer una altra
+    brigada per acabar de deixar la platja neta perquè hi puguem tornar un any
+    més!
   </p>
   <p>
     Moltes gràcies per cooperar perquè sigui una gran festa.<br>
@@ -28,11 +30,7 @@
     <b style=letter-spacing:4px>Que comenci l'estiu!</b>
   </p>
   <!--
-    <img src=img/cartell.jpg style="display:block;margin:auto;margin-bottom:10px;max-width:100%;box-shadow: 0 0 10px #000000">
-    <div style=font-size:smaller>
-      <div>* Cartell: Mireia Vilapuig</div>
-      <div>* Web: Lluís Bosch</div>
-    </div>
   -->
+  <img src=img/cartell2019.png style="display:block;margin:auto;margin-bottom:10px;max-width:100%;box-shadow: 0 0 10px #000000">
 
 </div>
