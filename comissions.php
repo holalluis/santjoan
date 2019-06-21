4808dbbd37dfbf6f15bcd3d15824466fa80b8922
holalluis <holalluis@gmail.com>
2019-06-22 00:31:27 +0200

    canvis

--- comissions.php
+++
@@ -1,9 +0,0 @@
-<!--
-<h3>Comissions</h3>
-<table border=1>
-  <tr><th>Comprar alcohol
-  <tr><th>Comprar gel
-  <tr><th>Llogar equip de so
-  <tr><th>~voluntaris a distribuir
-</table>
--->
--- index.php
+++ index.php
@@ -3,32 +3,37 @@
 </head><body>
 <?php include'navbar.php'?>
 <?php include'btn_join.php'?>
-<?php include'assistents.php'?>
-<?php include'comissions.php'?>
-<div style="max-width:50em;text-align:justify;text-justify:inter-word">
-  <p>
-    Aquesta festa la muntem perquè ens ve de gust, per celebrar el foc, el
-    solstici i la vida.
-  </p>
-  <p>
-    <b>Sentiu-vos partíceps i amfitrions de la festa, que és vostra</b>.
-    No sou al Sònar ni a L'Apol·lo: ningú es lucra amb la vostra celebració i això no té preu.
-    O un de molt petit: <b>us demanem que sigueu actius en cooperar perquè tot surti bé</b>
-    (des de muntar durant el transcurs de la festa i <b>sobretot al desmuntar</b>).
-  </p><p>
-    Un cop s'acabi la música s'haurà de portar tot el material electrònic a les
-    furgonetes, plegar les taules i la carpa i fer una primera recollida de
-    brossa. I quan la ressaca ens comenci a despertar haurem de fer una altra
-    brigada per acabar de deixar la platja neta perquè hi puguem tornar un any
-    més!
-  </p>
-  <p>
-    Moltes gràcies per cooperar perquè sigui una gran festa.<br>
-  </p>
-  <p>
-    <b style=letter-spacing:4px>Que comenci l'estiu!</b>
-  </p>
-  <!--
-  -->
-  <img src=img/cartell2019.png style="display:block;margin:auto;margin-bottom:10px;max-width:100%;box-shadow: 0 0 10px #000000">
-</div>
+
+
+<main class=flex>
+  <div>
+    <?php include'assistents.php'?>
+  </div>
+  <div style="max-width:50em;text-align:justify;text-justify:inter-word">
+    <p>
+      Aquesta festa la muntem perquè ens ve de gust, per celebrar el foc, el
+      solstici i la vida.
+    </p>
+    <p>
+      <b>Sentiu-vos partíceps i amfitrions de la festa, que és vostra</b>.
+      No sou al Sònar ni a L'Apol·lo: ningú es lucra amb la vostra celebració i això no té preu.
+      O un de molt petit: <b>us demanem que sigueu actius en cooperar perquè tot surti bé</b>
+      (des de muntar durant el transcurs de la festa i <b>sobretot al desmuntar</b>).
+    </p><p>
+      Un cop s'acabi la música s'haurà de portar tot el material electrònic a les
+      furgonetes, plegar les taules i la carpa i fer una primera recollida de
+      brossa. I quan la ressaca ens comenci a despertar haurem de fer una altra
+      brigada per acabar de deixar la platja neta perquè hi puguem tornar un any
+      més!
+    </p>
+    <p>
+      Moltes gràcies per cooperar perquè sigui una gran festa.<br>
+    </p>
+    <p>
+      <b style=letter-spacing:4px>Que comenci l'estiu!</b>
+    </p>
+    <!--
+    -->
+    <img src=img/cartell2019.png style="display:block;margin:auto;margin-bottom:10px;max-width:100%;box-shadow: 0 0 10px #000000">
+  </div>
+</main>
