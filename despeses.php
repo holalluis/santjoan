fb8e516649b31090ea973eedf6249f85394d856b
holalluis <holalluis@gmail.com>
2019-06-21 17:33:12 +0200

    canvis

--- despeses.php
+++ despeses.php
@@ -11,29 +11,28 @@
 
 <script>
   let Despeses = [
-    {q:2000, concepte:"compra gran begudes + menjar + cava + coca"},
-    {q: 375, concepte:"lloguer equip de so capsa trons"},
-    {q: 200, concepte:"música: 4 pau dub"},
-    {q: 200, concepte:"música: 3 trio rumba"},
-    {q: 150, concepte:"música: 3 el proyecto"},
-    {q: 150, concepte:"comissió decoració"},
-    {q: 100, concepte:"gestió jaume"},
-    {q: 100, concepte:"diners gots reutilitzables"},
-    {q: 100, concepte:"música: 2 neus+guitarra"},
-    {q: 100, concepte:"música: 1 dj lluis"},
-    {q:  80, concepte:"gel"},
-    {q:  60, concepte:"cubells"},
-    {q:  50, concepte:"benzina motor burra"},
-    {q:  50, concepte:"viatge furgo alcohol"},
-    {q:  50, concepte:"viatge equip de so"},
-    {q:  50, concepte:"música: 1 dj sara vidal"},
-    {q:  50, concepte:"feina rincón de la mierda + caixes"},
-    {q:  40, concepte:"viatge neus+guitarra"},
-    {q:  40, concepte:"viatge el proyecto"},
-    {q:  40, concepte:"viatge trio rumba"},
-    {q:  40, concepte:"viatge grup pau dub"},
-    {q:  30, concepte:"comprar caixes porex"},
-    {q:  23, concepte:"domini web"},
+    {q:2000, pagat:true, concepte:"compra gran begudes + menjar + cava + coca"},
+    {q: 375, pagat:false, concepte:"lloguer equip de so capsa trons"},
+    {q: 300, pagat:false, concepte:"música: 3 trio rumba"},
+    {q: 150, pagat:false, concepte:"música: 3 el proyecto"},
+    {q: 350, pagat:false, concepte:"comissió decoració"},
+    {q: 100, pagat:true, concepte:"gestió jaume"},
+    {q: 100, pagat:false, concepte:"diners gots reutilitzables"},
+    {q: 100, pagat:false, concepte:"música: 2 neus+guitarra"},
+    {q: 100, pagat:false, concepte:"música: 1 dj lluis"},
+    {q:  80, pagat:false, concepte:"gel"},
+    {q:  60, pagat:false, concepte:"cubells"},
+    {q:  50, pagat:true, concepte:"benzina motor burra"},
+    {q:  50, pagat:false, concepte:"viatge furgo alcohol"},
+    {q:  50, pagat:false, concepte:"viatge equip de so"},
+    {q:  50, pagat:false, concepte:"música: 1 dj sara vidal"},
+    {q:  50, pagat:false, concepte:"feina rincón de la mierda + caixes"},
+    {q:  40, pagat:false, concepte:"viatge neus+guitarra"},
+    {q:  40, pagat:false, concepte:"viatge el proyecto"},
+    {q:  40, pagat:false, concepte:"viatge trio rumba"},
+    {q:  40, pagat:false, concepte:"viatge grup pau dub"},
+    {q:  30, pagat:false, concepte:"comprar caixes porex"},
+    {q:  23, pagat:false, concepte:"domini web"},
   ];
 
   let table_despeses = document.querySelector('#despeses');
@@ -41,6 +40,7 @@
     let nr = table_despeses.insertRow(-1);
     nr.insertCell(-1).outerHTML=`<td align=right>${d.q}</td>`;
     nr.insertCell(-1).innerHTML=d.concepte;
+    nr.insertCell(-1).innerHTML=d.pagat?"pagat":"";
   });
   //total
   let total = Object.values(Despeses).map(d=>d.q).reduce((p,c)=>p+c);
