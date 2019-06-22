a8e2661d0d234b80d7f5f26327ac2613e7bb0b53
holalluis <holalluis@gmail.com>
2019-06-22 17:12:19 +0200

    canvis

--- despeses.php
+++ despeses.php
@@ -41,7 +41,7 @@
     total     : {q:    0, descr:"TOTAL despeses previstes (pagades + no pagades)" },
     pagat     : {q:    0, descr:"Despeses pagades " },
     no_pagat  : {q:    0, descr:"Despeses no pagades"},
-    banc      : {q: 1900, descr:"Diners al banc actualment" },
+    banc      : {q: 1940, descr:"Diners al banc actualment" },
     benefici  : {q:    0, descr:"Previsió diners sobrants i/o imprevistos (banc - no-pagat)"},
     ingressat : {q:    0, descr:"Ingressat total real (banc + pagat)"},
   };
