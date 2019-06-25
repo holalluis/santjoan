1b6e5fd8ffd0598aff460523cbb9ef6990e2bdfa
holalluis <holalluis@gmail.com>
2019-06-25 14:53:27 +0200

    canvis

--- despeses.php
+++ despeses.php
@@ -14,32 +14,29 @@
 <table id=despeses border=1></table>
 <script>
   let Despeses = [
-    {q:2000, pagat:true, concepte:"compra gran begudes + menjar + cava + coca"},
-    {q: 250, pagat:true, concepte:"comissió decoració"},
-    {q: 100, pagat:true, concepte:"gestió festa"},
-    {q:  60, pagat:true, concepte:"cubells"},
-    {q:  50, pagat:true, concepte:"benzina motor burra"},
-
-    {q: 375, pagat:false, concepte:"lloguer equip de so capsa trons"},
-    {q: 300, pagat:false, concepte:"música: 3 trio rumba"},
-    {q: 150, pagat:false, concepte:"música: 3 el proyecto"},
-    {q: 100, pagat:false, concepte:"música: 2 neus+guitarra"},
-    {q: 100, pagat:false, concepte:"música: 1 dj fast fingers"},
-    {q:  80, pagat:false, concepte:"gel"},
-
-    {q:  50, pagat:false, concepte:"viatge equip de so (diana pagada per lluís)"},
-    {q:  50, pagat:false, concepte:"comissió gots"},
-    {q:  50, pagat:false, concepte:"transport alcohol (manel D)"},
-    {q:  30, pagat:false, concepte:"transport alcohol (edu M)"},
-    {q:  30, pagat:false, concepte:"transport alcohol (max P)"},
-
-    {q:  50, pagat:false, concepte:"música: 1 dj sara vidal"},
-    {q:  50, pagat:false, concepte:"feina rincón de la mierda + caixes"},
-    {q:  40, pagat:false, concepte:"viatge neus+guitarra"},
-    {q:  40, pagat:false, concepte:"viatge el proyecto"},
-    {q:  40, pagat:false, concepte:"viatge trio rumba"},
-    {q:  30, pagat:false, concepte:"comprar caixes porex"},
-    {q:  23, pagat:false, concepte:"domini web"},
+    {q:2000, pagat:true, concepte:"compra gran begudes + menjar + cava + coca"}, //ES51 2100 0027 2002 0135 0511 -- max prat
+    {q: 340, pagat:true, concepte:"música: 3 trio rumba"},                       //ES45 2100 0412 7301 0036 7389 -- goyo
+    {q: 250, pagat:true, concepte:"comissió decoració"},                         //ES26 3025 0014 0714 0008 3269 -- georgina nicolás
+    {q: 200, pagat:true, concepte:"música: 3 el proyecto"},                      //ES85 0182 8730 0102 0091 3346 -- pau (el proyecto)
+    {q: 100, pagat:true, concepte:"gestió festa"},                               //ES24 2100 0665 4301 0090 1339 -- jaume madaula
+    {q:  60, pagat:true, concepte:"cubells"},                                    //ES86 3025 0002 4714 3341 4006 -- laia gausà
+    {q:  50, pagat:true, concepte:"benzina motor burra"},                        //ES24 2100 0665 4301 0090 1339 -- jaume madaula
+    {q:  50, pagat:true, concepte:"rincón de la mierda"},                        //ES13 2100 2904 0902 1832 4019 -- maria andreu hayles
+    {q:  50, pagat:true, concepte:"transport so+alcohol (manel)"},               //ES18 2100 0695 6101 0034 0733 -- manel dalmases
+    {q:  35, pagat:true, concepte:"gel"},                                        //                    metàl·lic -- gerard codolà
+    {q:  30, pagat:true, concepte:"transport so+alcohol (edu)"},                 //ES19 3025 0014 0014 0008 4417 -- edu madaula
+    {q:  30, pagat:true, concepte:"transport so+alcohol (max)"},                 //ES51 2100 0027 2002 0135 0511 -- max prat
+    {q:  30, pagat:true, concepte:"transport so+alcohol (jaume)"},               //ES24 2100 0665 4301 0090 1339 -- jaume madaula
+    {q:  30, pagat:true, concepte:"transport so+alcohol (yves)"},                //ES43 1491 0001 2021 3501 9723 -- yves dimant
+    {q: 375, pagat:false, concepte:"lloguer equip de so capsa trons"},           // -- capsa de trons
+    {q: 140, pagat:false, concepte:"música + viatge: neus+guitarra"},            // -- neus
+    {q: 100, pagat:false, concepte:"música: dj fast fingers"},                   // -- jo
+    {q:  50, pagat:false, concepte:"comissió gots"},                             // -- duna
+    {q:  50, pagat:false, concepte:"música: 1 dj sara vidal"},                   // -- sara vidal
+    {q:  40, pagat:false, concepte:"viatge equip de so"},                        // -- jo
+    {q:  30, pagat:false, concepte:"transport coses (lio)"},                     // -- lio
+    {q:  30, pagat:false, concepte:"comprar caixes porex"},                      //aclarir si es van comprar
+    {q:  23, pagat:false, concepte:"domini web"},                                // -- jo
   ];
 
   //totals
