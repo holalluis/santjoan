faabe27681f7a3cae6a59ad31b25bf5a9b146a36
holalluis <holalluis@gmail.com>
2019-06-26 16:28:12 +0200

    canvis

--- despeses.php
+++ despeses.php
@@ -14,30 +14,29 @@
 <table id=despeses border=1></table>
 <script>
   let Despeses=[
-    {q:2000, pagat:true, destí:"ES51 2100 0027 2002 0135 0511", concepte:"compra gran begudes + menjar + cava + coca"}, //max prat
-    {q: 340, pagat:true, destí:"ES45 2100 0412 7301 0036 7389", concepte:"música: 3 trio rumba"},                       //goyo
-    {q: 303, pagat:true, destí:"ES52 2100 0658 0101 0044 3754", concepte:"dj+viatge+web+gestió+cash diana"},            //lluís bosch
-    {q: 250, pagat:true, destí:"ES26 3025 0014 0714 0008 3269", concepte:"comissió decoració"},                         //georgina nicolás
-    {q: 208, pagat:true, destí:"ES36 1491 0001 2921 3274 5825", concepte:"transport coses + alcohol (lio)"},            //lio dimant
-    {q: 200, pagat:true, destí:"ES85 0182 8730 0102 0091 3346", concepte:"música: 3 el proyecto"},                      //pau (el proyecto)
-    {q: 100, pagat:true, destí:"ES24 2100 0665 4301 0090 1339", concepte:"gestió festa"},                               //jaume madaula
-    {q:  60, pagat:true, destí:"ES86 3025 0002 4714 3341 4006", concepte:"cubells"},                                    //laia gausà
-    {q:  50, pagat:true, destí:"ES24 2100 0665 4301 0090 1339", concepte:"benzina motor burra"},                        //jaume madaula
-    {q:  50, pagat:true, destí:"ES13 2100 2904 0902 1832 4019", concepte:"rincón de la mierda"},                        //maria andreu hayles
-    {q:  50, pagat:true, destí:"ES18 2100 0695 6101 0034 0733", concepte:"transport so+alcohol (manel)"},               //manel dalmases
-    {q:  50, pagat:true, destí:"ES97 2100 0298 5001 0159 5092", concepte:"música: 1 dj sara"},                          //sara vidal
-    {q:  35, pagat:true, destí:"en metàl·lic",                  concepte:"gel"},                                        //gerard codolà
-    {q:  30, pagat:true, destí:"ES19 3025 0014 0014 0008 4417", concepte:"transport so+alcohol (edu)"},                 //edu madaula
-    {q:  30, pagat:true, destí:"ES51 2100 0027 2002 0135 0511", concepte:"transport so+alcohol (max)"},                 //max prat
-    {q:  30, pagat:true, destí:"ES24 2100 0665 4301 0090 1339", concepte:"transport so+alcohol (jaume)"},               //jaume madaula
-    {q:  30, pagat:true, destí:"ES43 1491 0001 2021 3501 9723", concepte:"transport so+alcohol (yves)"},                //yves dimant
-    {q:  30, pagat:true, destí:"en metàl·lic",                  concepte:"caixes porex"},                               //ho va comprar gerard codolà
-    {q:  17, pagat:true, destí:"ES86 3025 0002 4714 3341 4006", concepte:"coca st joan"},                               //laia gausà
-
-    {q: 375, pagat:false, destí:"ES49 0081 0561 1400 0154 7063", concepte:"lloguer equip de so capsa trons"},           //-- capsa de trons
-    {q: 140, pagat:false, destí:"?",                             concepte:"música+viatge: neus i miquel"},              //-- neus
-    {q:  50, pagat:false, destí:"?",                             concepte:"comissió gots"},                             //-- duna
-    {q:  25, pagat:false, destí:"?",                             concepte:"devolucions no assistents"},                 //-- miquel felip peig
+    {q:2000, pagat:true,  destí:"ES51 2100 0027 2002 0135 0511", concepte:"compra gran begudes + menjar + cava + coca"}, //max prat
+    {q: 340, pagat:true,  destí:"ES45 2100 0412 7301 0036 7389", concepte:"música: 3 trio rumba"},                       //goyo
+    {q: 303, pagat:true,  destí:"ES52 2100 0658 0101 0044 3754", concepte:"dj+web+gestió+cash diana"},                   //lluís bosch
+    {q: 250, pagat:true,  destí:"ES26 3025 0014 0714 0008 3269", concepte:"comissió decoració"},                         //georgina nicolás
+    {q: 208, pagat:true,  destí:"ES36 1491 0001 2921 3274 5825", concepte:"transport coses + alcohol (lio)"},            //lio dimant
+    {q: 200, pagat:true,  destí:"ES85 0182 8730 0102 0091 3346", concepte:"música: 3 el proyecto"},                      //pau (el proyecto)
+    {q: 100, pagat:true,  destí:"ES24 2100 0665 4301 0090 1339", concepte:"gestió festa"},                               //jaume madaula
+    {q:  60, pagat:true,  destí:"ES86 3025 0002 4714 3341 4006", concepte:"cubells"},                                    //laia gausà
+    {q:  50, pagat:true,  destí:"ES24 2100 0665 4301 0090 1339", concepte:"benzina motor burra"},                        //jaume madaula
+    {q:  50, pagat:true,  destí:"ES13 2100 2904 0902 1832 4019", concepte:"rincón de la mierda"},                        //maria andreu hayles
+    {q:  50, pagat:true,  destí:"ES18 2100 0695 6101 0034 0733", concepte:"transport so+alcohol (manel)"},               //manel dalmases
+    {q:  50, pagat:true,  destí:"ES97 2100 0298 5001 0159 5092", concepte:"música: 1 dj sara"},                          //sara vidal
+    {q:  35, pagat:true,  destí:"metàl·lic",                     concepte:"gel"},                                        //gerard codolà
+    {q:  30, pagat:true,  destí:"ES19 3025 0014 0014 0008 4417", concepte:"transport so+alcohol (edu)"},                 //edu madaula
+    {q:  30, pagat:true,  destí:"ES51 2100 0027 2002 0135 0511", concepte:"transport so+alcohol (max)"},                 //max prat
+    {q:  30, pagat:true,  destí:"ES24 2100 0665 4301 0090 1339", concepte:"transport so+alcohol (jaume)"},               //jaume madaula
+    {q:  30, pagat:true,  destí:"ES43 1491 0001 2021 3501 9723", concepte:"transport so+alcohol (yves)"},                //yves dimant
+    {q:  30, pagat:true,  destí:"metàl·lic",                     concepte:"caixes porex"},                               //ho va comprar gerard codolà
+    {q:  17, pagat:true,  destí:"ES86 3025 0002 4714 3341 4006", concepte:"coca st joan"},                               //laia gausà
+    {q: 375, pagat:false, destí:"ES49 0081 0561 1400 0154 7063", concepte:"lloguer equip de so capsa trons"},             //-- capsa de trons
+    {q: 140, pagat:false, destí:"?",                             concepte:"música+viatge: neus i miquel"},                //-- neus
+    {q:  50, pagat:false, destí:"?",                             concepte:"comissió gots"},                               //-- duna
+    {q:  25, pagat:false, destí:"?",                             concepte:"devolucions no assistents (1)"},               //-- miquel felip peig
   ];
 
   //totals
@@ -45,7 +44,7 @@
     total    :{q:   0, descr:"TOTAL despeses previstes (pagades + no pagades)" },
     pagat    :{q:   0, descr:"Despeses pagades " },
     no_pagat :{q:   0, descr:"Despeses no pagades"},
-    banc     :{q: 967, descr:"Diners al banc actualment" },
+    banc     :{q: 987, descr:"Diners al banc actualment" },
     ingressat:{q:   0, descr:"Ingressat total real (banc + pagat)"},
     benefici :{q:   0, descr:"Previsió diners sobrants i/o imprevistos (banc - no-pagat)"},
   };
@@ -63,7 +62,7 @@
       let nr=table_despeses.insertRow(-1);
       nr.insertCell(-1).outerHTML=`<td class=number>${d.q}`;
       nr.insertCell(-1).innerHTML=d.concepte;
-      nr.insertCell(-1).innerHTML=d.pagat?"pagat":"<b>no pagat</b>";
+      nr.insertCell(-1).innerHTML=d.pagat?"<b style=color:lightgreen>pagat</b>":"<b style=color:red>no pagat</b>";
       nr.insertCell(-1).innerHTML=d.destí;
     });
     //totals
